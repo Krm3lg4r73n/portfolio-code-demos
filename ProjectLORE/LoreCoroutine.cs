db867280c00b57ea3dd9ecda4b45d37beb2b7084
David Czaplinski <david.czaplinski@ymail.com>
2016-07-17 20:18:40 +0200

    added Files

---
+++ ProjectLORE/LoreCoroutine.cs
@@ -0,0 +1,449 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Lore
+{
+    public enum ELoreCoroutineState
+    {
+        None,
+        Poked,
+        Returned,
+        Finished,
+        Cancelled
+    }
+
+    public class LoreCoroutine<T>
+    {
+
+        private bool m_Paused = false;
+
+        public ELoreCoroutineState state { get { return m_State; } }
+        private ELoreCoroutineState m_State = ELoreCoroutineState.None;
+
+        public T value { get { return m_ReturnVal; } }
+        private T m_ReturnVal = default(T);
+
+        public System.Exception exception { get { return m_Exception; } }
+        private System.Exception m_Exception = null;
+
+        public bool isRunning
+        {
+            get
+            {
+                return m_State == ELoreCoroutineState.None ||
+                        m_State == ELoreCoroutineState.Poked;
+            }
+        }
+
+        public IEnumerator routineState;
+
+
+        public LoreCoroutine(IEnumerator coroutine)
+        {
+            routineState = this.InternalRoutine(coroutine);
+        }
+
+        public void Cancel()
+        {
+            if (state == ELoreCoroutineState.Returned ||
+                state == ELoreCoroutineState.Finished)
+                return;
+            m_State = ELoreCoroutineState.Cancelled;
+        }
+
+        public void Poke()
+        {
+            if (state == ELoreCoroutineState.Returned ||
+                state == ELoreCoroutineState.Finished ||
+                state == ELoreCoroutineState.Cancelled)
+                return;
+            m_State = ELoreCoroutineState.Poked;
+        }
+
+
+        private IEnumerator InternalRoutine(IEnumerator coroutine)
+        {
+            m_ReturnVal = default(T);
+            m_Exception = null;
+            m_State = ELoreCoroutineState.None;
+            m_Paused = false;
+
+            //wait one frame for (other/previous) cancelled routines to finish
+            yield return null;
+
+            if (m_State == ELoreCoroutineState.Cancelled)
+            {
+                //routine was cancelled before it could start
+                m_Exception = new CoroutineCancelledException();
+                yield break;
+            }
+
+            while (true)
+            {
+                //inspect yield
+                object yielded = coroutine.Current;
+                if (yielded != null)
+                {
+                    if (yielded is T)
+                    {
+                        m_ReturnVal = (T)yielded;
+                        m_State = ELoreCoroutineState.Returned;
+                        yield break;
+                    }
+                    else if (yielded is LoreYieldInstruction)
+                    {
+                        var yieldInstruction = (LoreYieldInstruction)yielded;
+                        if (m_State == ELoreCoroutineState.Cancelled)
+                        {
+                            yieldInstruction.OnStateChange(ELoreCoroutineState.Cancelled);
+                            yieldInstruction.Cleanup();
+                        }
+                        else if (m_State == ELoreCoroutineState.Poked)
+                        {
+                            m_State = ELoreCoroutineState.None;
+                            yieldInstruction.OnStateChange(ELoreCoroutineState.Poked);
+                        }
+
+                        if (yieldInstruction.ResumeCoroutine())
+                        {
+                            m_Paused = false;
+                            if (m_State != ELoreCoroutineState.Cancelled)
+                                yieldInstruction.Cleanup();
+                        }
+                        else
+                        {
+                            m_Paused = true;
+                        }
+                    }
+                }
+
+                //move coroutine
+                try
+                {
+                    if (!m_Paused && !coroutine.MoveNext())
+                    {
+                        m_State = ELoreCoroutineState.Finished;
+                        yield break;
+                    }
+                }
+                catch (System.Exception e)
+                {
+                    DebugHelper.LogError(e.Message);
+                    m_Exception = e;
+                    yield break;
+                }
+
+                //cancel routine
+                if (m_State == ELoreCoroutineState.Cancelled)
+                {
+                    m_Exception = new CoroutineCancelledException();
+                    yield break;
+                }
+
+                yield return coroutine.Current;
+            }
+        }
+    }
+
+
+    public abstract class LoreYieldInstruction
+    {
+        public bool wasFired
+        {
+            get { return _wasFired; }
+            protected set { _wasFired = value; }
+        }
+        private bool _wasFired = false;
+
+        public abstract bool ResumeCoroutine();
+        public virtual void OnStateChange(ELoreCoroutineState state) { }
+        public virtual void Cleanup() { }
+    }
+
+    public class YieldAnd : LoreYieldInstruction
+    {
+        private LoreYieldInstruction lhs;
+        private LoreYieldInstruction rhs;
+
+        public YieldAnd(LoreYieldInstruction _lhs, LoreYieldInstruction _rhs)
+        {
+            lhs = _lhs;
+            rhs = _rhs;
+        }
+
+        public override bool ResumeCoroutine()
+        {
+            if (lhs.ResumeCoroutine() && rhs.ResumeCoroutine())
+                this.wasFired = true;
+            return this.wasFired;
+        }
+
+        public override void Cleanup()
+        {
+            lhs.Cleanup();
+            rhs.Cleanup();
+        }
+
+        public override void OnStateChange(ELoreCoroutineState state)
+        {
+            lhs.OnStateChange(state);
+            rhs.OnStateChange(state);
+        }
+    }
+
+    public class YieldOr : LoreYieldInstruction
+    {
+        private LoreYieldInstruction lhs;
+        private LoreYieldInstruction rhs;
+
+        public YieldOr(LoreYieldInstruction _lhs, LoreYieldInstruction _rhs)
+        {
+            lhs = _lhs;
+            rhs = _rhs;
+        }
+
+        public override bool ResumeCoroutine()
+        {
+            if (lhs.ResumeCoroutine() || rhs.ResumeCoroutine())
+                this.wasFired = true;
+            return this.wasFired;
+        }
+
+        public override void Cleanup()
+        {
+            lhs.Cleanup();
+            rhs.Cleanup();
+        }
+
+        public override void OnStateChange(ELoreCoroutineState state)
+        {
+            lhs.OnStateChange(state);
+            rhs.OnStateChange(state);
+        }
+    }
+
+    public class YieldNot : LoreYieldInstruction
+    {
+        private LoreYieldInstruction lhs;
+
+        public YieldNot(LoreYieldInstruction _lhs)
+        {
+            lhs = _lhs;
+        }
+
+        public override bool ResumeCoroutine()
+        {
+            if (!lhs.ResumeCoroutine())
+                this.wasFired = true;
+            return this.wasFired;
+        }
+
+        public override void Cleanup()
+        {
+            lhs.Cleanup();
+        }
+
+        public override void OnStateChange(ELoreCoroutineState state)
+        {
+            lhs.OnStateChange(state);
+        }
+    }
+
+    public class WaitForTimer : LoreYieldInstruction
+    {
+        private float timeStarted;
+        private float seconds;
+
+        public WaitForTimer(float _seconds)
+        {
+            timeStarted = Time.time;
+            seconds = _seconds;
+        }
+
+        public void Reset()
+        {
+            this.wasFired = true;
+            timeStarted = Time.time;
+        }
+
+        public override bool ResumeCoroutine()
+        {
+            if ((Time.time - timeStarted) > seconds)
+                this.wasFired = true;
+            return this.wasFired;
+        }
+    }
+
+    public class WaitForEvent<T> : LoreYieldInstruction where T : ILoreEvent
+    {
+        public T eventData;
+
+        private EventManager m_EventManager;
+        private bool m_Registered;
+
+
+        public WaitForEvent(EventManager _evntManager)
+        {
+            m_EventManager = _evntManager;
+            m_Registered = false;
+
+            this.eventData = default(T);
+        }
+
+        private void OnT(ILoreEvent arg0)
+        {
+            this.wasFired = true;
+            eventData = (T)arg0;
+        }
+
+        public void Reset()
+        {
+            this.wasFired = false;
+            this.eventData = default(T);
+        }
+
+        public override void Cleanup()
+        {
+            if (!GameManager.isQuitting)
+            {
+                m_EventManager.UnregisterListener(typeof(T), OnT);
+                m_Registered = false;
+            }
+        }
+
+        public override bool ResumeCoroutine()
+        {
+            if (!m_Registered)
+            {
+                m_EventManager.RegisterListener(typeof(T), OnT);
+                m_Registered = true;
+            }
+
+            return this.wasFired;
+        }
+    }
+
+    public class WaitForStateChange : LoreYieldInstruction
+    {
+        public ELoreCoroutineState state { get; private set; }
+
+        public override void OnStateChange(ELoreCoroutineState state)
+        {
+            this.state = state;
+            this.wasFired = true;
+        }
+
+        public void Reset()
+        {
+            this.wasFired = false;
+            this.state = ELoreCoroutineState.None;
+        }
+
+        public override bool ResumeCoroutine()
+        {
+            return this.wasFired;
+        }
+    }
+
+    public class WaitForCSharpEvent : LoreYieldInstruction
+    {
+        public WaitForCSharpEvent()
+        {
+        }
+
+        public void OnCSharpEvent()
+        {
+            this.wasFired = true;
+        }
+
+        public void Reset()
+        {
+            this.wasFired = false;
+        }
+
+        public override bool ResumeCoroutine()
+        {
+            return this.wasFired;
+        }
+    }
+
+    public class WaitForCSharpEvent<T> : LoreYieldInstruction
+    {
+        public T arg0;
+
+        public WaitForCSharpEvent()
+        {
+            this.arg0 = default(T);
+        }
+
+        public void OnCSharpEvent(T arg0)
+        {
+            this.wasFired = true;
+            this.arg0 = arg0;
+        }
+
+        public void Reset()
+        {
+            this.wasFired = false;
+            this.arg0 = default(T);
+        }
+
+        public override bool ResumeCoroutine()
+        {
+            return this.wasFired;
+        }
+    }
+
+    public class WaitForCSharpEvent<T0, T1> : LoreYieldInstruction
+    {
+        public T0 arg0;
+        public T1 arg1;
+
+        public WaitForCSharpEvent()
+        {
+            this.arg0 = default(T0);
+            this.arg1 = default(T1);
+        }
+
+        public void OnCSharpEvent(T0 arg0, T1 arg1)
+        {
+            this.wasFired = true;
+            this.arg0 = arg0;
+            this.arg1 = arg1;
+        }
+
+        public void Reset()
+        {
+            this.wasFired = false;
+            this.arg0 = default(T0);
+            this.arg1 = default(T1);
+        }
+
+        public override bool ResumeCoroutine()
+        {
+            return this.wasFired;
+        }
+    }
+
+    public class WaitForCoroutine : LoreYieldInstruction
+    {
+        private IEnumerator m_Routine;
+
+        public WaitForCoroutine(IEnumerator routine)
+        {
+            m_Routine = routine;
+        }
+
+        public override bool ResumeCoroutine()
+        {
+            wasFired = !m_Routine.MoveNext();
+            return wasFired;
+        }
+    }
+
+
+    public class CoroutineCancelledException : System.Exception
+    {
+        public CoroutineCancelledException() : base("Coroutine was cancelled") { }
+    }
+}
---
+++ ProjectLORE/Module_Inventory.cs
@@ -0,0 +1,214 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lore
+{
+    [RequireComponent(typeof(TriggerObject))]
+    [AddComponentMenu("Lore NetObject/Modules/Inventory")]
+    public sealed class Module_Inventory : ModuleBase, IInteractable, ITriggerProvider
+    {
+        [SerializeField]
+        private SphereCollider m_InteractionSphere;
+        public SphereCollider interactionSphere
+        {
+            get { return m_InteractionSphere; }
+        }
+
+        [SerializeField]
+        private InventoryReference m_InventoryRef;
+        public ushort inventoryId { get { return m_InventoryRef.id; } }
+
+        private List<uint> m_InteractingActors;
+
+        private Events_Trigger.ItemAdded e_ItemAdded;
+        private Events_Trigger.ItemRemoved e_ItemRemoved;
+        private Messages.ModifyInventory m_ModifyPayload;
+
+        public event System.Action<uint> onStart_Server;
+        public event System.Action<uint> onStop_Server;
+
+        public ETriggerObjectType mask
+        {
+            get { return ETriggerObjectType.Interaction | ETriggerObjectType.Inventory; }
+        }
+
+
+        //Shared
+        private void Awake()
+        {
+            if(GameManager.i.server != null)
+            {
+                m_InteractingActors = new List<uint>();
+                e_ItemAdded = new Events_Trigger.ItemAdded();
+                e_ItemAdded.inventoryId = m_InventoryRef.id;
+                e_ItemRemoved = new Events_Trigger.ItemRemoved();
+                e_ItemRemoved.inventoryId = m_InventoryRef.id;
+                m_ModifyPayload = new Messages.ModifyInventory();
+
+                GameManager.i.itemSystem.RegisterInventoryHandlers(m_InventoryRef.id,
+                    OnItemAddedRemoved, OnInventoryUpdated);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (GameManager.isQuitting)
+                return;
+
+            if (GameManager.i.server != null)
+            {
+                GameManager.i.itemSystem.UnregisterInventoryHandlers(m_InventoryRef.id,
+                    OnItemAddedRemoved, OnInventoryUpdated);
+            }
+        }
+
+        public override void HandleIdentityMessage(Messages.IdentityMessage.Payload payload)
+        {
+            if (GameManager.i.client != null)
+            {
+                if (payload is Messages.OpenInventory)
+                {
+                    var data = payload as Messages.OpenInventory;
+                    GlobalEventManager.i.InvokeEvent(new Events_UI.OpenInventory
+                    {
+                        type = EInventoryType.World,
+                        inventory = data.inventory
+                    });
+                }
+                else if (payload is Messages.StopInteration)
+                {
+                    GlobalEventManager.i.InvokeEvent(new Events_UI.CloseInventory
+                    {
+                        type = EInventoryType.World
+                    });
+                }
+                else if (payload is Messages.ModifyInventory)
+                {
+                    var data = payload as Messages.ModifyInventory;
+                    GlobalEventManager.i.InvokeEvent(new Events_UI.ModifyInventory
+                    {
+                        type = EInventoryType.World,
+                        slotIndex = data.slotIndex,
+                        itemRef = data.itemRef
+                    });
+                }
+            }
+        }
+
+
+        //Server
+        private void OnItemAddedRemoved(ItemReference itemRef, EInvModification mod)
+        {
+            if (m_TriggerEvents)
+            {
+                switch (mod)
+                {
+                    case EInvModification.Added:
+                        e_ItemAdded.itemReference = itemRef;
+                        GlobalEventManager.i.InvokeEvent(e_ItemAdded);
+                        break;
+                    case EInvModification.Removed:
+                        e_ItemRemoved.itemReference = itemRef;
+                        GlobalEventManager.i.InvokeEvent(e_ItemRemoved);
+                        break;
+                }
+            }
+        }
+
+        public void OnInventoryUpdated(short slotIndex, ItemReference itemRef)
+        {
+            foreach (var actor in m_InteractingActors)
+            {
+                var playerData = GameManager.i.server.playerManager.GetPlayerData(actor);
+                if (playerData != null)
+                {
+                    m_ModifyPayload.slotIndex = slotIndex;
+                    m_ModifyPayload.itemRef = itemRef;
+                    this.SendIdentityMessage(playerData.connectionId, m_ModifyPayload);
+                }
+            }
+        }
+
+        public void RequestPlayerInteraction(uint playerNetId)
+        {
+            var pd = GameManager.i.server.playerManager.GetPlayerData(playerNetId);
+            if (pd != null)
+            {
+                pd.player.RequestInteraction(this);
+            }
+        }
+
+        public void OnStartInteraction(IEnumerable<uint> actorIds)
+        {
+            foreach (var actorNetId in actorIds)
+            {
+                if (this.onStart_Server != null)
+                    this.onStart_Server(actorNetId);
+
+                if (!m_InteractingActors.Contains(actorNetId))
+                    this.m_InteractingActors.Add(actorNetId);
+                
+                if (m_TriggerEvents)
+                {
+                    GlobalEventManager.i.InvokeEvent(new Events_Trigger.InteractionStateChanged
+                    {
+                        state = Events_Trigger.InteractionStateChanged.EState.InteractionStarted,
+                        interactable = this.netIdentity,
+                        actor = GameManager.i.server.GetNetworkIdentity(actorNetId),
+                        tool = ItemReference.CreateInvalid()
+                    });
+                }
+
+                //send inventory to client if actor is player
+                var playerData = GameManager.i.server.playerManager.GetPlayerData(actorNetId);
+                if (playerData != null)
+                {
+                    var inv = GameManager.i.itemSystem.GetInventory(m_InventoryRef.id);
+
+                    DebugHelper.Assert(inv != null,
+                        "Object '" + gameObject.name + "' has an invalid inventory id.");
+
+                    this.SendIdentityMessage(playerData.connectionId,
+                        new Messages.OpenInventory { inventory = inv });
+                }
+            }
+        }
+
+        public void OnStopInteraction(IEnumerable<uint> actorIds)
+        {
+            foreach (var actorNetId in actorIds)
+            {
+                if (this.onStop_Server != null)
+                    this.onStop_Server(actorNetId);
+
+                if (m_InteractingActors.Contains(actorNetId))
+                    this.m_InteractingActors.Remove(actorNetId);
+
+                var playerData = GameManager.i.server.playerManager.GetPlayerData(actorNetId);
+                if (playerData != null)
+                    this.SendIdentityMessage(playerData.connectionId, new Messages.StopInteration());
+            }
+        }
+
+        public void StopAllPlayerInteractions()
+        {
+            var toRemove = new List<uint>();
+            foreach (var actorNetId in m_InteractingActors)
+            {
+                var playerData = GameManager.i.server.playerManager.GetPlayerData(actorNetId);
+                if (playerData != null)
+                {
+                    if (this.onStop_Server != null)
+                        this.onStop_Server(actorNetId);
+
+                    toRemove.Add(actorNetId);
+                    this.SendIdentityMessage(playerData.connectionId, new Messages.StopInteration());
+                }
+            }
+
+            foreach (var item in toRemove)
+                m_InteractingActors.Remove(item);
+        }
+    }
+}
